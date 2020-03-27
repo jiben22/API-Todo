d50bf382af953df8e30fedeb3f0c4208a2198ea8
jiben22 <benji.legallo22@live.fr>
2020-03-27 10:13:58 +0100

    Create swagger doc

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Todo
 Task management application - NodeJS
+
+## Documentation - Swagger
+
+[http://localhost:5000/api-docs](http://localhost:5000/api-docs)
+
--- app.js
+++ app.js
@@ -3,12 +3,16 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import router from './routes/index.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 // Set up the express app
 const app = express();
+// Set up Swagger docs
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = 5000;
 

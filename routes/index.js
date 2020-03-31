daefd65083593d4449a58ef0a01a3c9586165574
jiben22 <benji.legallo22@live.fr>
2020-03-31 19:16:11 +0200

    Fix Swagger api-docs URL

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ import express from 'express';
 import DefaultController from '../controller/defaultController';
 import TagController from '../controller/tagController';
 import TodoController from '../controller/todoController';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 
 const router = express.Router();
 
@@ -13,12 +15,16 @@ router.get('/api/v1/todos/:id', TodoController.getTodo);
 router.post('/api/v1/todos', TodoController.createTodo);
 router.put('/api/v1/todos/:id', TodoController.updateTodo);
 router.delete('/api/v1/todos/:id', TodoController.deleteTodo);
+
 /* Tags */
 router.get('/api/v1/tags', TagController.getTags);
 router.get('/api/v1/tags/:id', TagController.getTag);
 router.post('/api/v1/tags', TagController.createTag);
 router.put('/api/v1/tags/:id', TagController.updateTag);
 router.delete('/api/v1/tags/:id', TagController.deleteTag);
+
+/* Swagger */
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 /* Default */
 router.get('*', DefaultController.default);
